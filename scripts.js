6d201cf6ec407b0f89bf22d4e23538dad02b22e5
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-10-15 17:07:25 -0300

    get form data and create new object

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
-// checks all changes that occur in amount input
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+// checks all changes that occur in amount input
 amount.oninput = () => {
   // replace any non-numeric value with nothing
   let value = amount.value.replace(/\D/g, "")
@@ -18,4 +21,21 @@ function formatCurrencyBRL(value) {
         currency: "BRL"
     })
    return value
+}
+
+// Capture the form submit event to get the values.
+form.onsubmit = (event) => {
+  // Prevents the browser's default behavior of reloading the page when we submit the form.
+  event.preventDefault()
+
+  // Creates an object with the details of the new expense.
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+  console.log(newExpense)
 }
