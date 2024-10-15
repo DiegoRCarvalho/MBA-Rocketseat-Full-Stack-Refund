abd1ea054635a0ccfff407db0398a05c7611ff53
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-10-15 06:21:33 -0300

    formatting the currency in brazilian real

--- scripts.js
+++ scripts.js
@@ -4,5 +4,18 @@ const amount = document.getElementById("amount")
 amount.oninput = () => {
   // replace any non-numeric value with nothing
   let value = amount.value.replace(/\D/g, "")
-  amount.value = value
+
+  // converts values ​​to cents
+  value = Number(value)/100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+// formats the value in BRL standard
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+   return value
 }
