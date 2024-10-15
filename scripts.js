8ba7bc3cd4c2b08406f519deedef0958aaecbbf5
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-10-15 05:55:36 -0300

    replace any non-numeric value in the amount input with nothing

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+// checks all changes that occur in amount input
 const amount = document.getElementById("amount")
 
-// checks all changes that occur in amount input
 amount.oninput = () => {
-
+  // replace any non-numeric value with nothing
+  let value = amount.value.replace(/\D/g, "")
+  amount.value = value
 }
